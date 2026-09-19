0d2adaa67e660617ed8f2c0e418d62d2c7de5622
Aravind <aravindchaudhary90@gmail.com>
2026-09-19 21:57:34 +0530

    Add closeMenu function to MobileMenu links

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -3,6 +3,14 @@
 import Link from "next/link";
 
 export default function MobileMenu() {
+  const closeMenu = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const details = event.currentTarget.closest("details");
+
+    if (details) {
+      details.open = false;
+    }
+  };
+
   return (
     <details className="mobile-menu">
       <summary aria-label="Open navigation menu">
@@ -10,14 +18,37 @@ export default function MobileMenu() {
       </summary>
 
       <div className="mobile-menu-panel">
-        <Link href="/investments">Investments</Link>
-        <Link href="/insurance">Insurance</Link>
-        <Link href="/loans">Loans</Link>
-        <Link href="/imp">IMP</Link>
-        <Link href="/swp">SWP</Link>
-        <Link href="/articles">Articles</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+        <Link href="/investments" onClick={closeMenu}>
+          Investments
+        </Link>
+
+        <Link href="/insurance" onClick={closeMenu}>
+          Insurance
+        </Link>
+
+        <Link href="/loans" onClick={closeMenu}>
+          Loans
+        </Link>
+
+        <Link href="/imp" onClick={closeMenu}>
+          IMP
+        </Link>
+
+        <Link href="/swp" onClick={closeMenu}>
+          SWP
+        </Link>
+
+        <Link href="/articles" onClick={closeMenu}>
+          Articles
+        </Link>
+
+        <Link href="/about" onClick={closeMenu}>
+          About
+        </Link>
+
+        <Link href="/contact" onClick={closeMenu}>
+          Contact
+        </Link>
       </div>
     </details>
   );
